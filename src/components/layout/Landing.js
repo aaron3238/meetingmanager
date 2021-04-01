16aefeddd54db0fb0ab5d02ee93fb446fcdcc129
aaron3238 <aprit540@live.kutztown.edu>
2021-03-31 22:40:54 -0400

    fixed landing spacing

--- src/components/layout/Landing.js
+++ src/components/layout/Landing.js
@@ -1,7 +1,7 @@
 import React, {Component} from "react";
 import LoginForm from "../forms/LoginForm.js"
 import RegisterForm from "../forms/RegisterForm.js"
-
+import Styles from '../../Body.module.css';
 export default class Landing extends Component{
     constructor(props){
         super(props);
@@ -9,7 +9,7 @@ export default class Landing extends Component{
 
     render(){
         return(
-            <div>
+            <div style={{ padding: "1rem" }} className={Styles.App}>
             <h2>Please register, or login.</h2>
             <RegisterForm setToken={this.props.setToken}/>
             {'  '}
